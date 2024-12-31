db4a290c14fe0db51e2c0981efbd2c2f5eb56578
MihajloVat <vatolkinmihajlo@gmail.com>
2024-12-31 21:36:33 +0200

    day (step 1)

--- Tasks/day.js
+++ Tasks/day.js
@@ -1,16 +1,18 @@
-// Refactor following solution
-// Get day number
+//STEP 1:
+//Fix naming.
+//Add "const"s.
+//Remove early "i" declaration.
 
-let D = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
 
-_parse_day_ = (s) => {
-  let i;
-  for (i = 0; i < D.length; i++) {
-    if (s.startsWith(D[i].toLowerCase())) {
-      return i + 1;
+const days = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
+
+const parseDay = (day) => {
+    for (let i = 0; i < days.length; i++) {
+        if (day.startsWith(days[i].toLowerCase())) {
+            return i + 1;
+        }
     }
-  }
-  return -1;
+    return -1;
 };
 
-module.exports = _parse_day_;
+module.exports = parseDay;
